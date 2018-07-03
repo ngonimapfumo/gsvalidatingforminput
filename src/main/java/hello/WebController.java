fd8407b2ddf8a79b21f5307c73bc9353e22da46c
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2018-07-03 18:44:58 +0200

    Initial commit

---
+++ src/main/java/hello/WebController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+public class WebController implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+}
